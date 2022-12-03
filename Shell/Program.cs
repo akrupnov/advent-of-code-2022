03a2918f254875fcc04c1b89f5966fc06532c46c
Alex Krupnov <alexkrupnov@microsoft.com>
2022-12-03 15:15:58 +0100

    Day 1 pt. 2

--- Shell/Program.cs
+++ Shell/Program.cs
@@ -3,7 +3,8 @@ using Solutions;
 
 
 
-Console.WriteLine("Day 1. Max total calories: " + new DayOne().Solve(File.ReadAllText("./input/day1"))) ;
+Console.WriteLine("Day 1. Max total calories: " + new DayOne(File.ReadAllText("./input/day1")).SolvePartOne()) ;
+Console.WriteLine("Day 1 pt. Top 3 total calories: " + new DayOne(File.ReadAllText("./input/day1")).SolvePartTwo()) ;
 
 
 
--- Solutions/DayOne.cs
+++ Solutions/DayOne.cs
@@ -2,10 +2,26 @@
 using System.Linq;
 public class DayOne
 {
-    public String Solve(String input)
+    private readonly string _input;
+
+    public DayOne(String input)
     {
-        var maxElveCalories = input.Split("\n\n").Select(x => x.Split("\n").Select(s => Convert.ToInt32(s))).Max(e => e.Sum());
+        this._input = input;
+    }
+    public String SolvePartOne()
+    {
+        var maxElveCalories = GetCalorieValues().Max(e => e.Sum());
 
         return maxElveCalories.ToString();
+    }
+
+    public String SolvePartTwo()
+    {
+        return GetCalorieValues().Select(e => e.Sum()).OrderDescending().Take(3).Sum().ToString();
+    }
+
+    private IEnumerable<IEnumerable<int>> GetCalorieValues()
+    {
+        return _input.Split("\n\n").Select(x => x.Split("\n").Select(s => Convert.ToInt32(s)));
     }
 }
