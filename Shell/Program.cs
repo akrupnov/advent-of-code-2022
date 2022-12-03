5b6c47c018c8a895141f619c4b804e33e8f78140
Alex Krupnov <alexkrupnov@microsoft.com>
2022-12-03 15:06:47 +0100

    Day 1 solution

---
+++ Shell/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Solutions;
+
+
+
+Console.WriteLine("Day 1. Max total calories: " + new DayOne().Solve(File.ReadAllText("./input/day1"))) ;
+
+
+
---
+++ Solutions/DayOne.cs
@@ -0,0 +1,11 @@
+﻿namespace Solutions;
+using System.Linq;
+public class DayOne
+{
+    public String Solve(String input)
+    {
+        var maxElveCalories = input.Split("\n\n").Select(x => x.Split("\n").Select(s => Convert.ToInt32(s))).Max(e => e.Sum());
+
+        return maxElveCalories.ToString();
+    }
+}
