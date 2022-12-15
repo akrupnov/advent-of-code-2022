1f7317329adb3f40ea69a0f13b53fc3b5a160ac2
Alex Krupnov <alexkrupnov@microsoft.com>
2022-12-15 22:07:53 +0100

    Day 12, pt. 1

--- Solutions/DayTwelve.cs
+++ Solutions/DayTwelve.cs
@@ -61,119 +61,57 @@ namespace Solutions
 
             var start = fieldDictionary.First(x => x.Value.Height == 'S').Value;
             start.Height = 'a';
+            var target = fieldDictionary.First(x => x.Value.Height == 'E').Value;
+            target.Height = 'z';
 
-            var count = BFSFindNodeWithStartNode(start);
-            //var path = FindShortestPathToEnd(start, new List<Field>(), new List<Field>());
-            throw new Exception();
+            var path = SolveDijkstra(start, target);
+            return (path.Count - 1).ToString();
         }
 
-        private void DijkstraSearch(Field start, ISet<Field> visited)
+    private static IList<Field> SolveDijkstra(Field start, Field? target)
+    {
+        var distances = new Dictionary<Field, Int32>();
+        var transitions = new Dictionary<Field, Field>();
+
+        var queueTracker = new List<Field>();
+        distances[start] = 0;
+        var workingQueue = new PriorityQueue<Field, int>();
+        workingQueue.Enqueue(start, distances[start]);
+        queueTracker.Add(start);
+        
+        while (workingQueue.Count > 0)
         {
-            //Start.MinCostToStart = 0;
-            var prioQueue = new List<Field>();
-            prioQueue.Add(start);
-            do {
-                prioQueue = prioQueue.OrderBy(x => x.Height).ToList();
-                var node = prioQueue.First();
-                prioQueue.Remove(node);
-
-                foreach (var childNode in node.AdjacentFields.Where(x => !visited.Contains(x) && (Convert.ToByte(x.Height) <= Convert.ToByte(node.Height) + 1 || x.Height == 'E') ).OrderBy(x => x.Height))
+            var u = workingQueue.Dequeue();
+            queueTracker.Remove(u);
+            if (u == target)
+                break;
+            foreach (var neighbour in u.AdjacentFields.Where(x => x.Height <= u.Height + 1))
+            {
+                var distanceToExplore =  distances[u] + 1;
+                if (distanceToExplore < (distances.ContainsKey(neighbour) ? distances[neighbour] : int.MaxValue))
                 {
-                    if (childNode.MinCostToStart == null ||
-                        node.MinCostToStart + cnn.Cost < childNode.MinCostToStart)
+                    distances[neighbour] = distanceToExplore;
+                    transitions[neighbour] = u;
+                    if (! queueTracker.Contains(neighbour))
                     {
-                        childNode.MinCostToStart = node.MinCostToStart + cnn.Cost;
-                        childNode.NearestToStart = node;
-                        if (!prioQueue.Contains(childNode))
-                            prioQueue.Add(childNode);
+                        workingQueue.Enqueue(neighbour, distanceToExplore);
+                        queueTracker.Add(neighbour);
                     }
                 }
-                node.Visited = true;
-                if (node == End)
-                    return;
-            } while (prioQueue.Any());
+            }
         }
-
-        private IList<Field> FindShortestPathToEnd(Field node, List<Field> visited, List<Field> pathToHere)
+        var path = new List<Field>();
+        if (transitions.ContainsKey(target) || target == start)
         {
-            Console.WriteLine($"{node.ToString()}");
-            pathToHere.Add(node);
-
-            if(node.Height == 'E')
+            while (target != null)
             {
-                return pathToHere;
+                path.Add(target);
+                target = transitions.ContainsKey(target) ? transitions[target] : null;
             }
-            else
-            {
-                visited.Add(node);
-            }
-            var childPaths = new List<IList<Field>>();
-
-            var adjustedAdjacent = node.AdjacentFields.Where(x => !visited.Contains(x) && (Convert.ToByte(x.Height) <= Convert.ToByte(node.Height) + 1 || x.Height == 'E') ).ToList();
-
-            foreach(var field in adjustedAdjacent)
-            {
-                var path = FindShortestPathToEnd(field, visited, new List<Field>());            
-                
-                var d = "d;";
-
-                if(path.Where(x => x.Height == 'E').Any())
-                {
-                    childPaths.Add(path);
-                }
-/*                else
-                {
-                    visited.RemoveAll(x => path.Contains(x));
-                }*/
-            }
-            var shortestPath = childPaths.OrderBy(x => x.Count()).FirstOrDefault();
-            if(shortestPath != null) 
-                pathToHere.AddRange(shortestPath);
-            return pathToHere;
         }
+        return path;
+    }
 
-        public int BFSFindNodeWithStartNode(Field startingPoint)
-            {
-                if (startingPoint.Height == 'E')
-                {
-                    return 0;
-                }
-
-                var visited = new HashSet<Field>();
-                visited.Add(startingPoint);
-                
-                var q = new Queue<Field>();
-                
-                // Add this node to the queue
-                q.Enqueue(startingPoint);
-                
-                int count = 0;
-                
-                while(q.Count > 0)
-                {
-                    var current = q.Dequeue();
-                    
-                    Console.WriteLine(current);
-                    if (current.Height == 'E')
-                    {
-                        return visited.Count();
-                    }
-                    
-                    // Iterate through UNVISITED nodes
-                    foreach(var neighbour in current.AdjacentFields.Where(x => !visited.Contains(x) && (Convert.ToByte(x.Height) <= Convert.ToByte(current.Height) + 1 || x.Height == 'E')))
-                    {
-                        if(neighbour.Height == 'E')
-                        {
-                            return visited.Count();
-                        }
-                        visited.Add(neighbour);
-                        q.Enqueue(neighbour);
-                    }
-                    count++;
-                }
-                Console.WriteLine("Could not find node!");
-                return count;
-            }        
         
         public override string SolvePartTwo()
         {
