e9bda27e6e12f2a639db90793f68f1434905f6cd
Alex Krupnov <alexkrupnov@microsoft.com>
2022-12-17 22:08:33 +0100

    Day 14, pt. 2

--- Solutions/DayFourteen.cs
+++ Solutions/DayFourteen.cs
@@ -143,6 +143,35 @@ namespace Solutions
                 }
             }
         }
+        private void DrawCave(Dictionary<(Int32, Int32), SpaceType> cave, int sandPieces)
+        {
+            return;
+            //Thread.Sleep(1);
+            Console.Clear();
+            Console.SetCursorPosition(70, 70);
+            Console.Write(sandPieces);
+            var minX = cave.Min(x => x.Key.Item1);
+            foreach(var caveField in cave)
+            {
+                Console.SetCursorPosition(caveField.Key.Item1 - minX, caveField.Key.Item2);
+                switch(caveField.Value)
+                {
+                    case SpaceType.Rock:
+                        Console.Write("#");
+                        break;
+                    case SpaceType.Empty:
+                        Console.Write(".");
+                        break;
+                    case SpaceType.Sand:
+                        Console.Write("o");
+                        break;
+                    case SpaceType.EverFloatingSand:
+                        Console.Write("~");
+                        break;
+                }
+            }
+            
+        }
 
         public override string SolvePartTwo()
         {
@@ -155,17 +184,9 @@ namespace Solutions
             var maxY = wallDefinitions.Select(x => x.Max(d => d.Item2)).Max(x => x);
 
             
-            var cave = Enumerable.Range(0, maxY + 3).
-                Select((row, ri) => Enumerable.Range(0, maxX - minX +1).Select(
-                    (c, ci) => new CaveSpace()
-                    {
-                        Coords = (ri, ci)
-                    }
-                    ).ToArray()
-                ).ToArray();
+            var cave = new Dictionary<(int, int), SpaceType>();
 
-            cave[cave.Length - 1].ToList().ForEach(x => x.SpaceType = SpaceType.Rock);
-            
+
             foreach(var wd in wallDefinitions.Select(x => x.ToArray()))
             {
                 for(var i = 0; i < wd.Length - 1; i++)
@@ -176,7 +197,7 @@ namespace Solutions
 
                         for(var c = yes.Min(); c <= yes.Max(); c++)
                         {
-                            cave[c][wd[i].Item1 - minX].SpaceType = SpaceType.Rock;
+                            cave[(wd[i].Item1, c)] = SpaceType.Rock;
                         }
                     }
                     else
@@ -184,53 +205,58 @@ namespace Solutions
                         var xes = new List<Int32>() {wd[i].Item1, wd[i + 1].Item1};
                         for(var c = xes.Min(); c <= xes.Max(); c++)
                         {
-                            cave[wd[i].Item2][c - minX].SpaceType = SpaceType.Rock;
+                            cave[(c, wd[i].Item2)] = SpaceType.Rock;
                         }
                     }
                 }
             }
             
-            var startCoord = (0, 500 - minX);
+            var startCoord = (500, 0);
+            cave[startCoord] = SpaceType.Empty;
             var sandPieces = 1;
             
             var coord = startCoord;
             while(true)
             {
-                try
+                if(true)
                 {
-                    if(true)
+                    for(var i = coord.Item1 - 1; i <= coord.Item1 + 1; i++)
                     {
-                        if(cave[coord.Item1 + 1][coord.Item2].SpaceType == SpaceType.Empty)
-                        {
-                            cave[coord.Item1][coord.Item2].SpaceType = SpaceType.Empty;
-                            coord = (coord.Item1 + 1, coord.Item2);
-                        }
-                        else if(cave[coord.Item1 + 1][coord.Item2 - 1].SpaceType == SpaceType.Empty)
-                        {
-                            cave[coord.Item1][coord.Item2].SpaceType = SpaceType.Empty;
-                            coord = (coord.Item1 + 1, coord.Item2 - 1);
-                        }
-                        else if(cave[coord.Item1 + 1][coord.Item2 + 1].SpaceType == SpaceType.Empty)
-                        {
-                            cave[coord.Item1][coord.Item2].SpaceType = SpaceType.Empty;
-                            coord = (coord.Item1 + 1, coord.Item2 + 1);
-                        }
-                        else
-                        {
-                            sandPieces++;
-                            coord = startCoord;
-                        }
-
+                        cave[(i, maxY + 2)] = SpaceType.Rock;
                     }
 
-                    cave[coord.Item1][coord.Item2].SpaceType = SpaceType.Sand;
+                    var bottomCoord = (coord.Item1, coord.Item2 + 1);
+                    var leftDiagCoord = (coord.Item1 - 1, coord.Item2 + 1);
+                    var rightDiagCoord = (coord.Item1 + 1, coord.Item2 + 1);
+
+                    if(!cave.ContainsKey(bottomCoord) || cave[bottomCoord] == SpaceType.Empty)
+                    {
+                        cave[coord] = SpaceType.Empty;
+                        coord = bottomCoord;
+                    }
+                    else if(!cave.ContainsKey(leftDiagCoord) || cave[leftDiagCoord] == SpaceType.Empty)
+                    {
+                        cave[coord] = SpaceType.Empty;
+                        coord = leftDiagCoord;
+                    }
+                    else if(!cave.ContainsKey(rightDiagCoord) || cave[rightDiagCoord] == SpaceType.Empty)
+                    {
+                        cave[coord] = SpaceType.Empty;
+                        coord = rightDiagCoord;
+                    }
+                    else
+                    {
+                        sandPieces++;
+                        if(coord == startCoord)
+                        {
+                            return (sandPieces - 1).ToString();
+                        }
+                        coord = startCoord;
+                    }
+                    
+                    cave[coord] = SpaceType.Sand;
                     DrawCave(cave, sandPieces);
                 }
-                catch(IndexOutOfRangeException)
-                {
-                    return (sandPieces - 1).ToString();
-                }
-
             }        
         }
     }
